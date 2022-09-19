255380bb2a0486c30dc337a03e22ce20912a52c7
Ernesto Bessone <ejubessone@gmail.com>
2022-09-19 12:46:22 -0300

    BaSP-W05-2022: Exercise 1(Variables and operators) added

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,17 @@
+// 1. A.
+var firstVar = 13;
+var secondVar = 7;
+var thirdVar = firstVar + secondVar;
+console.log('1.A.: ' + thirdVar);
+
+// 1. B.
+var nombre = 'Esteban';
+var apellido = 'Quito';
+var FullName = nombre + ' ' + apellido;
+console.log('1.B.: ' + FullName);
+
+// 1. C.
+var firstString = 'Radium'
+var secondString = 'Rocket'
+var thirdString = firstString.length + secondString.length;
+console.log('1.C.: ' + 'cantidad de letras ' + thirdString);
